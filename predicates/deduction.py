46a27d31649efb03733ab155a14c009dd933e939
bar371 <33864527+BarCohen1@users.noreply.github.com>
2018-01-08 13:42:12 +0200

    working on task_1

--- predicates/deduction.py
+++ predicates/deduction.py
@@ -19,8 +19,32 @@ def inverse_mp(proof, assumption, print_as_proof_forms=False):
     assert type(assumption) is str
     assert Schema(assumption) in proof.assumptions
     assert proof.assumptions[:len(Prover.AXIOMS)] == Prover.AXIOMS
-
+    print('org proof assumptions:', proof.assumptions)
+    print('our kilshon' , assumption)
+    new_proof_assump = copy.deepcopy(proof.assumptions)
+    del new_proof_assump[Schema(new_proof_assump.index(assumption))]
+    new_proof = Proof(assumption, proof.conclusion, [])
     # divide to cases and run recurs
+    for line in proof.lines:
+        l_type = line.justification[0]
+        if l_type == 'A':
+            n
+        elif l_type == 'T':
+            pass
+        elif l_type == 'MP':
+            pass
+        elif l_type == 'UG':
+            pass
+
+    # CASE1 line just is T    sol: T -> assumption -> T ; do MP and we get assumption -> T
+    # CASE2 line just is A    solution1: A -> assumption -> A; do MP and we get assumption -> A (when A!=assumption)
+                              #solution2: A == assumption -> ; add_tautology(assumption)
+    # CASE3 line just is MP   solution: find assumption -> (A->B) , find assumption -> A in prev lines
+                                        # use tau_inf to get assumption -> B
+    # CASE4 line just is UG   solution: add UG  Ax[assumption -> cur] , when x is the same var used in org line
+                                        # add US Ax[assumption -> cur] -> (assumption -> Ax[cur])
+                                        # add MG to get assumption -> Ax[cur]
+
     # Task 11.1
 
 def proof_by_contradiction(proof, assumption, print_as_proof_forms=False):
