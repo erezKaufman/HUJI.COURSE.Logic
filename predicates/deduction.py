b136c939d0192473fd93c6807001f5dee0252362
bar371 <33864527+BarCohen1@users.noreply.github.com>
2018-01-08 12:09:45 +0200

    new 11 stuff

---
+++ predicates/deduction.py
@@ -0,0 +1,39 @@
+""" (c) This file is part of the course
+    Mathematical Logic through Programming
+    by Gonczarowski and Nisan.
+    File name: code/predicates/deduction.py """
+
+from predicates.syntax import *
+from predicates.proofs import *
+from predicates.prover import *
+
+def inverse_mp(proof, assumption, print_as_proof_forms=False):
+    """ Takes a proof, whose first six assumptions/axioms are Prover.AXIOMS, of
+        a conclusion from a list of assumptions/axioms that contains the given
+        assumption as a simple formula (i.e., without any templates), where no
+        step of the proof is a UG step over a variable that is free in the
+        given assumption, and returns a proof of (assumption−>conclusion) from
+        the same assumptions except assumption """
+    assert type(proof) is Proof
+    assert proof.is_valid()
+    assert type(assumption) is str
+    assert Schema(assumption) in proof.assumptions
+    assert proof.assumptions[:len(Prover.AXIOMS)] == Prover.AXIOMS
+
+    # divide to cases and run recurs
+    # Task 11.1
+
+def proof_by_contradiction(proof, assumption, print_as_proof_forms=False):
+    """ Takes a proof, whose first six assumptions/axioms are Prover.AXIOMS, of
+        a contradiction (a formula whose negation is a tautology)  a list of
+        assumptions/axioms that contains the given assumption as a simple
+        formula (i.e., without any templates), where no step of the proof is a
+        UG step over a variable that is free in the given assumption, and
+        returns a proof of ~assumption from the same assumptions except
+        assumption """
+    assert type(proof) is Proof
+    assert proof.is_valid()
+    assert type(assumption) is str
+    assert Schema(assumption) in proof.assumptions
+    assert proof.assumptions[:len(Prover.AXIOMS)] == Prover.AXIOMS
+    # Task 11.2
